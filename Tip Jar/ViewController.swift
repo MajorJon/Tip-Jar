179c1b37b1f2687f6fc9bfceab1dda9a02095506
user150601 <user150601@la213.local>
2019-02-10 11:28:00 -0800

    Initial Commit

---
+++ Tip Jar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Jar
+//
+//  Created by user150601 on 2/10/19.
+//  Copyright © 2019 jtmajor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
