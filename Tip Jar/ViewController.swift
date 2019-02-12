eda0e3acf53b171c909f95f558b489e3076a5b67
user150601 <user150601@la213.local>
2019-02-11 21:05:13 -0800

    Init Commit \

--- Tip Jar/ViewController.swift
+++ Tip Jar/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipTotalView: UIView!
+    @IBOutlet weak var billField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+        performSegue(withIdentifier: "mySegue", sender: self)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        //calculate bill
+        let bill = Double(billField.text!) ?? 0
+        
+        let tipPercentage = [0.15, 0.20, 0.25]
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
